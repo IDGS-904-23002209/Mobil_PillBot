48f8e540267edfd9598a498b149fcd0d4fb067a4
Gbariela Paola Perez Ornelas <perezornelasgabrielapaola@gmail.com>
2026-07-02 15:52:50 -0600

    login

--- app/src/main/java/com/jimenaoropeza/pillbot/data/modelo/LoginRequest.kt
+++ app/src/main/java/com/jimenaoropeza/pillbot/data/modelo/LoginRequest.kt
@@ -2,8 +2,5 @@ package com.jimenaoropeza.pillbot.modelo
 
 data class LoginRequest(
     val correo: String,
-    val contrasena: String,
-    val dispositivo: String,
-    val ipOrigen: String,
-    val detallesNavegador: String
+    val contrasena: String
 )
