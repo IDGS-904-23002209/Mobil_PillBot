4cdf8347bbe3c9b1a57c9a8bfd8c2c8fdf2297e7
IDGS-904_23002209 <oropezajim11@gmail.com>
2026-08-06 01:55:20 -0600

    Logo listo JO 06/08/2026

--- app/src/main/java/com/jimenaoropeza/pillbot/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/jimenaoropeza/pillbot/data/network/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://kdrgs23g-7046.use.devtunnels.ms/"
+    private const val BASE_URL = "http://pillbot-back.somee.com/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
